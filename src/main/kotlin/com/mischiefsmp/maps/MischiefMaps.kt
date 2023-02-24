3e655c9d49d1a3031958a264f3e6e8575baa1a70
SvenWollinger <accounts@wollinger.email>
2023-02-24 11:45:38 +0100

    Update MischiefMaps.kt

--- src/main/kotlin/com/mischiefsmp/maps/MischiefMaps.kt
+++ src/main/kotlin/com/mischiefsmp/maps/MischiefMaps.kt
@@ -1,11 +1,16 @@
 package com.mischiefsmp.maps
 
+import com.mischiefsmp.maps.commands.ImageMapCommand
 import org.bukkit.plugin.java.JavaPlugin
 
 class MischiefMaps: JavaPlugin() {
 
     init {
         plugin = this
+    }
+
+    override fun onEnable() {
+        getCommand("imagemap")!!.setExecutor(ImageMapCommand())
     }
 
     companion object {
